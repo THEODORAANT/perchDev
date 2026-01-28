de9d2e8b3f27c677c78fafe2a58222fa6bec388d
theoMooblu <perchrunway@gmail.com>
2026-01-28 11:55:54 +0200

    perch

--- shop/products/index.php
+++ shop/products/index.php
@@ -3,7 +3,9 @@
     include('../../perch/runtime.php');
 ?>
 <?php
-
+  perch_shop_product($product_slug, [
+            'template' => 'products/shop-product.html',
+          ]);
     perch_shop_product(perch_get("s"),[
                                           'template' => 'products/product_view.html',
                                           'variants' => true,
