4b518cc13c9956b57c6df924c23cffa42a7bf23d
THEODORAANT <35761172+THEODORAANT@users.noreply.github.com>
2026-04-01 18:46:51 +0300

    Upgrade Perch CKEditor integration to CKEditor 4 LTS CDN

--- perch/addons/plugins/editors/ckeditor/perch/env.js
+++ perch/addons/plugins/editors/ckeditor/perch/env.js
@@ -1,2 +1,3 @@
-var CKEDITOR_BASEPATH = Perch.path + '/addons/plugins/editors/ckeditor/ckeditor-4/';
+// Use CKEditor LTS from CDN instead of the bundled legacy 4.8.0 build.
+var CKEDITOR_BASEPATH = 'https://cdn.ckeditor.com/4.24.0-lts/standard-all/';
 $('.ckeditor').removeClass('ckeditor').addClass('perch-ckeditor');
